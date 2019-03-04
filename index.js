b04a729a5ebdd70304bd98118f7b06276e8a7103
Michael Dilley <mike.dilley@gmail.com>
2019-03-04 12:05:30 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function addComment() {
+  event.preventDefault();
+  let bodyText = document.getElementById("commentText").value;
+  let commenter = document.getElementById("commenterName").value;
+
+  let commentBody = createCommentBody(bodyText);
+  let commenterLabel = createCommenterLabel(commenter);
+  postNewComment(commentBody, commenterLabel);
+}
+
+function createCommentBody(comment) {
+  let bodyDiv = document.createElement("div");
+  let bodyPara = document.createElement("p");
+  bodyPara.innerHTML = comment;
+  bodyDiv.appendChild(bodyPara);
+  return bodyDiv;
+}
+
+function createCommenterLabel(commenter) {
+  let commenterDiv = document.createElement("div");
+  let commenterLabel = document.createElement("p");
+  commenterLabel.innerHTML = "posted by: ";
+  let commenterName = document.createElement("span");
+  commenterName.innerHTML = commenter;
+  commenterLabel.appendChild(commenterName);
+  commenterDiv.appendChild(commenterLabel);
+  return commenterDiv;
+}
+
+function postNewComment(body, commenter) {
+  let commentsDiv = document.getElementById("comments");
+  let newCommentDiv = document.createElement("div");
+  newCommentDiv.appendChild(body);
+  newCommentDiv.appendChild(commenter);
+  commentsDiv.appendChild(newCommentDiv);
+}
